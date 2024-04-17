d98440832234381df9dc810d04f11bbc2f058703
Maria Herculana <112400121+Mhercu@users.noreply.github.com>
2024-04-17 19:39:01 -0300

    Cardapio.java
    atualização

--- demo/src/main/java/com/example/Aplicações/Cardapio.java
+++ demo/src/main/java/com/example/Aplicações/Cardapio.java
@@ -3,22 +3,28 @@ package com.example.Aplicações;
 import java.util.ArrayList;
 import java.util.List;
 
+// Classe que representa um cardápio semanal
 public class Cardapio {
     
+    // Lista de itens do cardápio
     public List<ItemCard> itens;
 
+    // Construtor padrão que inicializa a lista de itens
     public Cardapio() {
         this.itens = new ArrayList<>();
     }
 
+    // Método para adicionar um item ao cardápio
     public void adicionarItem(ItemCard item) {
         itens.add(item);
     }
 
+    // Método para remover um item do cardápio
     public void removerItem(ItemCard item) {
         itens.remove(item);
     }
 
+    // Método para exibir o cardápio na tela
     public void exibir() {
         System.out.println("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         System.out.println("CARDÁPIO SEMANAL:");
@@ -29,6 +35,7 @@ public class Cardapio {
         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
     }
 
+    // Método para buscar um item pelo dia da semana
     public ItemCard buscarItem(String nome) {
         for (ItemCard item : itens) {
             if (item.getDia().equalsIgnoreCase(nome)) {
